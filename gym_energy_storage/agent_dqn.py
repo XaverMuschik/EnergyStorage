added plotting to dqn agent including bugfix regarding normalization of observation space before predicting

gym_energy_storage/agent_dqn.py
@@ -20,20 +20,20 @@ class Agent:
         self.observations = self.env.observation_space
         self.actions = len(self.env.action_space)
         # DQN Agent Variables
-        self.replay_buffer_size = 50_000  # ToDo: tune hyperparameter
+        self.replay_buffer_size = 10_000  # ToDo: tune hyperparameter
         self.train_start = 1_000  # ToDo: tune hyperparameter
         self.memory: Deque = collections.deque(maxlen=self.replay_buffer_size)
         self.gamma = 1  # 0.95
         self.epsilon = 1.0
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.05
+        self.epsilon_decay = 0.999
         # DQN Network Variables
         self.state_shape = self.observations
         self.learning_rate = 1e-3
         self.dqn = DQN(self.state_shape, self.actions, self.learning_rate)
         self.target_dqn = DQN(self.state_shape, self.actions, self.learning_rate)
         self.target_dqn.update_model(self.dqn)
-        self.batch_size = 32
+        self.batch_size = 128
 
     def get_action(self, state: np.ndarray):
         if np.random.rand() <= self.epsilon:
@@ -107,6 +107,12 @@ class Agent:
                 q_values[i][a] = rewards[i] + self.gamma * np.max(q_values_next[i])
 
         self.dqn.fit(self.normalize(states), q_values)
+        self.plot()
+
+    def plot(self):
+        """ plot result of training"""
+        plot = plot_learning_result(stor_val=40, mean_price=40, model=self.dqn)
+        plot.main()
 
     def play(self, num_episodes: int, render: bool = True):
         self.dqn.load_model(MODEL_PATH)

gym_energy_storage/plot_learning_outcome.py
@@ -32,6 +32,18 @@ class plot_learning_result():
         self.checkerboard_table(data)
         plt.show()
 
+    def normalize(self, state):
+        """ normalizes observations to interval [0,1] """
+        def scale(min_arg, max_arg, arg):
+            return (arg - min_arg) / (max_arg - min_arg)
+
+        normalized = state.copy()
+        normalized[0, 0] = scale(0.0, env.len_period, state[0, 0])  # normalize time step
+        normalized[0, 1] = scale(-200.0, 200.0, state[0, 1])  # normlize price
+        normalized[0, 2] = scale(0.0, env.max_stor_lev, state[0, 1])  # normlize storage level
+        normalized[0, 3] = scale(-200.0, 200.0, state[0, 3])  # normalize storage value
+        normalized[0, 4] = scale(-100.0, 100.0, state[0, 4])  # normalize mean price
+        return normalized
 
     def checkerboard_table(self, data, fmt='{:.0f}'):
         fig, ax = plt.subplots()
@@ -50,7 +62,7 @@ class plot_learning_result():
 
         def calc_values(time_step, price, stor_vol):
             observations = np.asarray([time_step, price, stor_vol, self.stor_val, self.mean_price]).reshape(1, -1)
-            return self.model(observations).numpy()[0]
+            return self.model(self.normalize(observations)).numpy()[0]
 
         # Add cells
         for stor_lev in data.columns: